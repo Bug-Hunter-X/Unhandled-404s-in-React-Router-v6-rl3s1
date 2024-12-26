4420c36dfb8fb7acd245b7e40321fdc65b17b14a
BugHunter-X <askzenai@gmail.com>
2024-12-26 14:02:05 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,19 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Missing Route for '*' */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+export default App;
+```
